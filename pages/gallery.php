754a48d7e54f469528217d61f7ef759d18b0f51e
Aureli-Rio <servant.aurelien.developer@gmail.com>
2023-05-09 21:55:48 +0200

    v.2

--- pages/gallery.php
+++ pages/gallery.php
@@ -16,12 +16,18 @@ $sql = "SELECT id_exhibitions, titre, date, lieu, image, categorie FROM exhibiti
 $result = mysqli_query($conn, $sql);
 
 // Vérification des erreurs lors de l'exécution de la requête
+if(!$result) {
+    echo "Erreur : " . mysqli_error($conn);
+    exit();
+}
+
+// Récupération des données de toutes les photos
 $photos = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 // Création d'un tableau associatif qui associe les catégories à leurs descriptions
 $categories = [
-    "art_digital" => "Art digital - Conceptuel, art moderne, travail sur Photoshop",
-    "noir_blanc" => "Noir & Blanc - Photos de voyage, scènes de vie, portraits"
+    "Art digital" => "Art digital - Conceptuel, art moderne, travail sur Photoshop",
+    "Noir et Blanc" => "Noir & Blanc - Photos de voyage, scènes de vie, portraits"
 ];
 
 // Parcours des photos et création d'un tableau associatif qui associe chaque catégorie à un tableau contenant les photos correspondantes
@@ -35,23 +41,34 @@ foreach ($photos as $photo) {
 }
 ?>
 
-<DOCTYPE html>
-<h2 class="secondary_title_style"><center>GALLERY</center></h2>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Galerie d'art</title>
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
 
-<?php
-// Parcours des catégories et affichage des photos correspondantes
-foreach ($categories as $categorie => $description) {
-    echo "<h3>$description</h3>";
-    echo "<ul id='gallery'>";
-    foreach ($photos_categorie[$categorie] as $photo) {
-        $id = $photo["id_exhibitions"];
-        $titre = $photo["titre"];
-        $date = $photo["date"];
-        $image = $photo["image"];
-        $categorie = $photo["categorie"];
-        echo "<li><a href='photo.php?id=$id'><br/><img src='$image' alt='$titre' width='500' height='500'><br/>$titre - $date</a></li>";
-    }
-    echo "</ul>";
-}
-include('../includes/footer.php');
-?>
+	<h2 class="secondary_title_style"><center>GALLERY</center></h2>
+
+	<?php
+	// Parcours des catégories et affichage des photos correspondantes
+	foreach ($categories as $categorie => $description) {
+	    echo "<h3>$description</h3>";
+	    echo "<ul id='gallery'>";
+	    foreach ($photos_categorie[$categorie] as $photo) {
+	        $id = $photo["id_exhibitions"];
+	        $titre = $photo["titre"];
+	        $date = $photo["date"];
+	        $lieu = $photo["lieu"];
+	        $image = $photo["image"];
+	        $categorie = $photo["categorie"];
+	        echo "<li><a href='photo.php?id=$id'><img src='$image' alt='$titre' width='100' height='100'><br/>$titre - $date - $lieu</a></li>";
+	    }
+	    echo "</ul>";
+	}
+	include('../includes/footer.php');
+	?>
+
+</body>
+</html>
