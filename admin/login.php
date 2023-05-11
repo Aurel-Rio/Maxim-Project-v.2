d1535f111bb6297b94b2dc2ea06b0743c317f3dc
Aureli-Rio <servant.aurelien.developer@gmail.com>
2023-05-11 22:51:54 +0200

    v.2

--- admin/login.php
+++ admin/login.php
@@ -2,7 +2,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require_once('../includes/config.php');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'maximarmengolcasino');
+define('DB_USER', 'riozacki');
+define('DB_PASSWORD', 'Domino.bae.713');
+
+$pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
 
 session_start();
 
@@ -37,7 +42,14 @@ if (isset($_POST['submit'])) {
     }
 
     // Si tout est bon, on redirige vers la page add_photo.php
-    header('Location: add_photo.php');
+    header('Location: http://127.0.0.1/MAXIM_ARMENGOL_CASINO_php_version/admin/add_photo.php');
     exit();
 }
-?>
+
+// Création d'un nouvel utilisateur 'MacSim' avec le mot de passe chiffré avec bcrypt
+$pseudo = 'MacSim';
+$mdp = 'MaCsIm.9.sav';
+$mdp_hash = password_hash($mdp, PASSWORD_BCRYPT);
+
+$stmt = $pdo->prepare("INSERT INTO admin (pseudo, mdp) VALUES (:pseudo, :mdp)");
+$stmt->execute(array(':pseudo' => $pseudo, ':mdp' => $mdp_hash));
