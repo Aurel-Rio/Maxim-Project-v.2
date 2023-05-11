241e7693b4ff02f7b8308fced3a0b3e22326ca07
Aureli-Rio <servant.aurelien.developer@gmail.com>
2023-05-11 14:10:55 +0200

    v.2

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,14 +1,33 @@
+window.onload = function() {
+  // votre code JavaScript ici
+  const navbar = document.querySelector("nav");
+  
+  if (window.location.pathname.includes("pages/accueil.php")) { // Vérifie si la page actuelle est la page d'accueil
+    window.addEventListener("scroll", function() {
+      const navbar = document.querySelector("nav");
+      if (window.scrollY > 0) {
+        navbar.classList.remove("big");
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+        navbar.classList.add("big");
+      }
+    });
+  }
+};
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    // votre code JavaScript ici
-    const navbar = document.querySelector("nav");
 
+
+/*
 window.addEventListener("scroll", function() {
-  if (window.pageYOffset > 0) {
+  const navbar = document.querySelector("nav");
+  if (window.scrollY > 0) {
+    navbar.classList.remove("big");
     navbar.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
+    navbar.classList.add("big");
   }
 });
-  });
+*/
--- includes/navbar.php
+++ includes/navbar.php
@@ -3,10 +3,10 @@
   <head>
 
   <link rel="stylesheet" href="/MAXIM_ARMENGOL_CASINO_php_version/assets/css/navbar.css">
-  <script src="/MAXIM_ARMENGOL_CASINO_php_version/assets/js/navbar.js"></script>
+  <script defer src="/MAXIM_ARMENGOL_CASINO_php_version/assets/js/navbar.js"></script>
   </head>
   <body>
-    <nav>
+    <nav class="big">
       <img id="logo_nav" src="/MAXIM_ARMENGOL_CASINO_php_version/assets/img/logo.jpg" alt="Logo Maxim">
       <h2>MAXIM CASINO ARMENGOL </h2>
       <ul>
