64e6b3fcb33bc6377a7b223ab26507dc1179dda3
Aureli-Rio <servant.aurelien.developer@gmail.com>
2023-05-11 15:00:54 +0200

    v.2

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,19 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
   const navbar = document.querySelector("nav");
-  
+
   if (window.location.pathname.includes("index.php")) {
     window.addEventListener("scroll", function() {
       if (window.pageYOffset > 0) {
         navbar.classList.remove("big");
+        navbar.classList.remove("no-transition");
         navbar.classList.add("scrolled");
       } else {
         navbar.classList.remove("scrolled");
+        navbar.classList.remove("no-transition");
         navbar.classList.add("big");
       }
     });
   } else {
-    navbar.classList.add("no-transition");
     navbar.classList.add("scrolled");
+    navbar.classList.add("no-transition");
+    const navbarElements = navbar.querySelectorAll("*");
+    for (let i = 0; i < navbarElements.length; i++) {
+      navbarElements[i].classList.add("no-transition");
+    }
   }
 });
 
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-
-  <link rel="stylesheet" href="/MAXIM_ARMENGOL_CASINO_php_version/assets/css/navbar.css">
-  <script defer src="/MAXIM_ARMENGOL_CASINO_php_version/assets/js/navbar.js"></script>
+    <link rel="stylesheet" href="/MAXIM_ARMENGOL_CASINO_php_version/assets/css/navbar.css">
+    <script defer src="/MAXIM_ARMENGOL_CASINO_php_version/assets/js/navbar.js"></script>
   </head>
   <body>
-  <nav class="big no-transition">
-      <img id="logo_nav" src="/MAXIM_ARMENGOL_CASINO_php_version/assets/img/logo.jpg" alt="Logo Maxim">
-      <h2>MAXIM CASINO ARMENGOL </h2>
+    <nav class="big no-transition">
+      <img  id="logo_nav" src="/MAXIM_ARMENGOL_CASINO_php_version/assets/img/logo.jpg" alt="Logo Maxim">
+      <h2> MAXIM CASINO ARMENGOL </h2>
       <ul>
         <li><a href="/MAXIM_ARMENGOL_CASINO_php_version/index.php">Accueil</a></li>
         <li><a href="/MAXIM_ARMENGOL_CASINO_php_version/pages/gallery.php">Galerie</a></li>
