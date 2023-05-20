43e43c9eeb21d2c98cf6b0838d40d1b79a95f843
Aureli-Rio <servant.aurelien.developer@gmail.com>
2023-05-20 15:22:51 +0200

    v.2

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 // Inclusion du fichier de configuration de la base de données
 require_once("./includes/config.php");
 
-//Affichage des erreurs
+// Affichage des erreurs
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+// Vérification de l'accès à la route "/admin"
+$url = $_SERVER['REQUEST_URI'];
+$file = basename($url);
+$isAdminPage = strpos($url, '/admin/') !== false && $file === 'login.php';
+if ($isAdminPage) {
+    // Vérifier si l'administrateur est connecté
+    if (!$adminConnected) {
+        // Rediriger l'utilisateur vers une autre page ou renvoyer une réponse d'erreur
+        header('Location: /');
+        exit;
+    }
+}
 
 // Chargement de la page d'accueil
 $page = isset($_GET['page']) ? $_GET['page'] : 'accueil';
@@ -22,4 +35,3 @@ switch ($page) {
         break;
 }
 ?>
-
